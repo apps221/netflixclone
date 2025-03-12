a544286a4380aeb62834aa33c230c48ae758ba25
apps221 <maggie.a.hong@gmail.com>
2025-03-12 09:16:37 -0400

    fixing searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import Search from './pages/Search/Search'
-import Loading from './pages/Search/Loading'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(()=> {
+onAuthStateChanged(auth, async (user)=> {
+  if(user) {
+    console.log("logged in")
+    navigate('/')
+  }
+  else {
+    console.log("logged out")
+    navigate('/login')
+  }
+})
+  }, [])
   return (
     <div>
+      <ToastContainer theme='dark'/>
       <Routes>
      <Route path='/' element={<Home />}></Route> 
      <Route path='/login' element={<Login />}></Route> 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/assets/logo.png'
 import search_icon from '../../assets/assets/search_icon.svg'
@@ -6,9 +6,21 @@ import bell_icon from '../../assets/assets/bell_icon.svg'
 import profile from '../../assets/assets/profile_img.png'
 import dropdown from '../../assets/assets/caret_icon.svg'
 import { Link } from 'react-router-dom'
+import { logout } from '../../firebase'
 const Navbar = () => {
+  const navRef = useRef();
+useEffect(()=> {
+  window.addEventListener('scroll', ()=> {
+    if(window.scrollY >= 80){
+      navRef.current.classList.add('nav-dark')
+    }
+    else {
+      navRef.current.classList.remove('nav-dark')
+    }
+  })
+})
   return (
-    <div className='navbar'>
+    <div ref={navRef} className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
@@ -28,7 +40,7 @@ const Navbar = () => {
           <img src={profile} alt="" className='profile'/>
           <img src={dropdown} alt="" />
           <div className="dropdown">
-            <p>Sign Out</p>
+            <p onClick={()=> {logout()}}>Sign Out</p>
           </div>
         </div>
       </div>
